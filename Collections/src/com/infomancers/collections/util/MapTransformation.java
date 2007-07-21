aa6be7ca1691eb19854e24c110e9f77b8502d0d7
aviad.infomancers <aviad.infomancers@6e8b00cd-7b35-0410-bcca-87c3f44085e8>
2007-07-21 11:15:41 +0000

    Added simple transformation wrappers for common java collections.
    git-svn-id: http://infomancers-collections.googlecode.com/svn/trunk@30 6e8b00cd-7b35-0410-bcca-87c3f44085e8

---
+++ Collections/src/com/infomancers/collections/util/MapTransformation.java
@@ -0,0 +1,58 @@
+package com.infomancers.collections.util;
+
+import java.util.Map;
+
+/**
+ * Copyright (c) 2007, Aviad Ben Dov
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * 3. Neither the name of Infomancers, Ltd. nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Used for simple transformations based on key-value pairs
+ * located in a standard Map.
+ *
+ * @see java.util.Map
+ */
+public final class MapTransformation<T, K> implements Transformation<T, K> {
+    private Map<T, K> map;
+
+    public MapTransformation(Map<T, K> map) {
+        this.map = map;
+    }
+
+    /**
+     * Transforms item of type T to item of type K.
+     *
+     * @param item The item to transform.
+     * @return The transformed item.
+     */
+    public K transform(T item) {
+        return map.get(item);
+    }
+}
