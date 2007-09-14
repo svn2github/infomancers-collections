60144e022a114acb71c09603f1726a5b2d126b71
aviad.infomancers <aviad.infomancers@6e8b00cd-7b35-0410-bcca-87c3f44085e8>
2007-09-14 10:43:10 +0000

    Added priority blocking queue, missing from the java.util.concurrent.
    git-svn-id: http://infomancers-collections.googlecode.com/svn/trunk@68 6e8b00cd-7b35-0410-bcca-87c3f44085e8

---
+++ Collections/testsrc/com/infomancers/tests/PriorityBlockingDequeTests.java
@@ -0,0 +1,11 @@
+package com.infomancers.tests;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aviadbd
+ * Date: Sep 14, 2007
+ * Time: 8:39:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PriorityBlockingDequeTests {
+}
